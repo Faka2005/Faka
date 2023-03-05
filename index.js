baeb99650e25cfa86acc535fcd532047f42e9fe6
Faka2005 <115738354+Faka2005@users.noreply.github.com>
2023-03-05 13:09:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const Bienvenue = 'ٱلسَّلَامُ عَلَيْكُمْ وَرَحْمَةُ ٱللَّٰهِ وَبَرَكَاتُهُ!' // Try edit me
+const Bienvenue1="Welcome"
+const Histoire=""
+const Ecouter=""
+const Juzz=""
+const Livre=""
+const Sourate=""
+const Prophete=""
+const Tadjweed=""
+
+// Update header text
+document.querySelector('#Presentation').innerHTML = Bienvenue;
+document.querySelector('#titre').innerHTML = Bienvenue1;
+document.querySelector('#Histoire').innerHTML = Histoire;
+document.querySelector('#Ecouter').innerHTML = Ecouter;
+document.querySelector('#Juzz').innerHTML = Juzz;
+document.querySelector('#Livre').innerHTML = Livre;
+document.querySelector('#Sourate').innerHTML = Sourate;
+document.querySelector('#Prophete').innerHTML = Prophete;
+document.querySelector('#Tadjweed').innerHTML = Tadjweed;
+;
